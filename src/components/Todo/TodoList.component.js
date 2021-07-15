d0c19ec9e298aa4e0d5ab380820d7f6aa4bec2a6
Master Dev <yvesmartialboah@gmail.com>
2021-07-15 20:40:18 +0000

    list data

--- src/components/Todo/TodoList.component.js
+++ src/components/Todo/TodoList.component.js
@@ -2,9 +2,8 @@
 /* eslint-disable quotes */
 /* eslint-disable semi */
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { View } from 'react-native';
-import {  } from "native-base"
+import React, { useState, useEffect } from 'react';
+import { View, FlatList } from 'react-native';
 import {
     Stack, 
     Input,
@@ -21,12 +20,65 @@ import {
   } from "native-base";
   import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Feather from 'react-native-vector-icons/Feather';
+import {updateTodoList, deleteTodoList, queryAllTodoList} from '../../database/db';
+import realm from '../../database/db';
 
 export default function TodoListComponent({navigation}) {
 
-    const [inputValue, setInputValue] = React.useState("");
+    const [todoLists, setTodoLists] = useState([
+        // {
+        //     id: new Date().getTime(),
+        //     name: 'xlm',
+        //     creationDate: '15-07-2021',
+        //     todos: [
+        //         {
+        //             id: 152,
+        //             name: 'manoush',
+        //             done: true,
+        //         }
+        //     ], // []
+        // },
+        // {
+        //     id: new Date().getTime() + 2,
+        //     name: 'Lorem',
+        //     creationDate: '15-07-2021',
+        //     todos: [
+        //         {
+        //             id: 152,
+        //             name: 'manoush',
+        //             done: true,
+        //         }
+        //     ], // []
+        // }
+    ]);
+    // console.log(todoLists,`todoListsXS`);
 
-    const addTodo = (title: string) => {}
+    const [inputValue, setInputValue] = useState("");
+
+    const addTodo = () => {}
+    const reloadData = () => {
+        queryAllTodoList().then((todoLists) => {
+            console.log(todoLists,`reloadDataX`);
+            setTodoLists(todoLists);
+        }).catch((error) => {
+            setTodoLists({ todoLists: [] });
+        });
+    }
+
+    // const Item = ({ name, lor }) => (
+    //     <View>
+    //       <Text>{name} {lor}</Text>
+    //     </View>
+    //   );
+
+    //     const renderItem = ({ item }) => (
+    //       <Item name={item.name} lor={item.lor} />
+    //     );
+          
+
+    useEffect(()=>{
+        reloadData();
+    },[])
 
     return (
         <NativeBaseProvider>
@@ -56,32 +108,42 @@ export default function TodoListComponent({navigation}) {
             value={inputValue}
             placeholder="Add Item"
           />
-          {/* <VStack>
-            {list.map((item, itemI) => (
-              <HStack
-                w="100%"
-                justifyContent="space-between"
-                alignItems="center"
-                key={item.title + itemI.toString()}
-              >
-                <Checkbox
-                  colorScheme="emerald"
-                  isChecked={item.isCompleted}
-                  onChange={() => handleStatusChange(itemI)}
-                  value={item.title}
-                >
-                  <Text mx={2} strikeThrough={item.isCompleted}>
-                    {item.title}
-                  </Text>
-                </Checkbox>
-                <IconButton
-                  colorScheme="emerald"
-                  icon={<Icon as={FontAwesome5} name="trash" size={5} />}
-                  onPress={() => handleDelete(itemI)}
-                />
-              </HStack>
-            ))}
-          </VStack> */}
+           {/* <FlatList
+                data={todoLists}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            /> */}
+
+            <VStack>
+            {todoLists != [] &&  (
+                  todoLists.map((item, id) => (
+                    <HStack
+                        w="100%"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        key={item.name + id.toString()}
+                    >
+                        <Checkbox
+                        colorScheme="emerald"
+                        isChecked={false}
+                        //   onChange={() => handleStatusChange(itemI)}
+                        value={item.name}
+                        >
+                        <Text mx={2} strikeThrough={item.isCompleted}>
+                            {item.name} - {item.creationDate}
+                        </Text>
+                        </Checkbox>
+                        <IconButton
+                        colorScheme="emerald"
+                        icon={<Icon as={FontAwesome5} name="trash" size={5} />}
+                        //   onPress={() => handleDelete(itemI)}
+                        />
+                    </HStack>
+                    ))
+            )}
+          
+            </VStack>
+
         </VStack>
         </Center>
        </NativeBaseProvider>
